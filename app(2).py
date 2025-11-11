d781cc78e69c7a109781ba50cfd484ae5a2791d4
malik8-beep <106090527+malik8-beep@users.noreply.github.com>
2025-11-11 21:01:38 +0000

    Add files via upload

---
+++ app(2).py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hello Streamlit depuis Colab")
+st.write("🚀 Ceci est un déploiement via ngrok")
+
+
+# Remplacer ici par ton propre authtoken
+conf.get_default().auth_token = "35Kjxwa0KMsM3xhQSJVnIMKPk2Z_2E18ihCB5KQbpZDQkACXy"
+
+
