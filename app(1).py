a26ad76f7142d900af4e6c1a189cfcc7640e3f8f
malik8-beep <106090527+malik8-beep@users.noreply.github.com>
2025-11-11 20:46:22 +0000

    Add files via upload

---
+++ app(1).py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hello Streamlit depuis Colab")
+st.write("🚀 Ceci est un déploiement via ngrok")
+
+# 3. Importer et configurer ngrok
+from pyngrok import ngrok, conf
+
+# Remplacer ici par ton propre authtoken
+conf.get_default().auth_token = "35Kjxwa0KMsM3xhQSJVnIMKPk2Z_2E18ihCB5KQbpZDQkACXy"
+
+
+
+# 5. Créer le tunnel
+public_url = ngrok.connect(8501)
+print("🚀 Lien vers votre app Streamlit :", public_url)
